1944eed9148ef8f6cd99af7e0b6e7a1b2438ca36
bnonni <bryan@nonni.io>
2024-09-14 15:41:17 -0400

    adding URI encoding

--- src/dpm.ts
+++ src/dpm.ts
@@ -23,12 +23,23 @@ export async function resolve(
     if(!(did && packageName && version)) {
       throw new Error('DMI resolution failed: invalid DMI format' + specifier);
     }
-    const drl = `https://dweb/${did}/read/records/package?filter.tags.name="${packageName}"&filter.tags.version="${version}"`;
+    const drl = `https://dweb/did:dht:sjqxjhiw6q7s6m4z34erj43e5w8dxztf6h9gdmtk767wpwt4a5ky/read/records/package?filter.tags.name="${packageName}"&filter.tags.version="${version}"`;
     console.log('DRL created', drl);
     const data = await fetchResource(did, drl, specifier);
     console.log('DPM resolved DMI to DWN record', data);
   }
   return defaultResolve(specifier, context, defaultResolve);
+}
+
+export function encodeURI(url: string) {
+  const baseUrl = 'https://dwn.nonni.org/did:dht:sjqxjhiw6q7s6m4z34erj43e5w8dxztf6h9gdmtk767wpwt4a5ky/query';
+  const filterTags = {
+    name    : 'tool5',
+    version : '1.0.2'
+  };
+  const queryString = `filter.tags.name=${encodeURIComponent(filterTags.name)}&filter.tags.version=${encodeURIComponent(filterTags.version)}`;
+  const encodedUrl = `${baseUrl}?${queryString}`;
+  console.log(encodedUrl);
 }
 
 export async function load(url: string, context: any, defaultLoad: Function) {
