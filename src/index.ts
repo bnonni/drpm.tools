c22c7fb574c0b0c24cdbddffa77fbc8019bfed04
bnonni <bryan@nonni.io>
2024-09-26 13:07:58 -0400

    committing changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './dwn/schema/package.js';
 export * from './dwn/protocol.js';
-export * from './dpm.js';
+export * from './hooks.js';
 export * from './utils/types.js';
 export * from './utils/utils.js';
