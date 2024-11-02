0fca1d73a8d2bcc64f598da706fe15811e8d543f
bnonni <github@nonni.dev>
2024-11-02 13:00:12 -0400

    resolve conflict

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,6 @@ import { DrlUtils } from './utils/dwn/drl-utils.js';
 import dwn from './utils/dwn/protocol.js';
 import { homedir } from 'os';
 
-<<<<<<< Updated upstream
-export const DRPM_REGISTRY_URL = `http://localhost:2092`;
-=======
 export const DRPM_HOME = `${process.env.HOME || homedir()}/.config/drpm`;
 export const DRPM_PROFILE = `${DRPM_HOME}/profile.json`;
 export const DEFAULT_WEB5DATAPATH = `${DRPM_HOME}/DATA`;
@@ -33,7 +30,6 @@ export const DEFAULT_PROFILE = {
 };
 export const DRPM_REGISTRY_PORT = process.env.PORT || '2092';
 export const DRPM_REGISTRY_URL = `http://localhost:${DRPM_REGISTRY_PORT}`;
->>>>>>> Stashed changes
 export const DRPM_DWN_URL = 'https://dwn.drpm.tools';
 export const DRPM_REGISTRY_DIR = join(process.env.HOME || homedir(), '.drpm', 'registry');
 export const DRPM_PROTOCOL_B64URL = DrlUtils.base64urlEncode(dwn.protocol);
